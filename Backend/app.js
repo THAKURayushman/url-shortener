089c5806eeff25aafe7e8a1fcc62882e8e80bc24
Ayushman Thakur <thakurayushman333@gmail.com>
2025-05-21 20:31:11 +0530

    port update

--- Backend/app.js
+++ Backend/app.js
@@ -35,9 +35,9 @@ app.get("/:id", redirectFromShortUrl);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  connectDB();
-  console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // GET - Redirection

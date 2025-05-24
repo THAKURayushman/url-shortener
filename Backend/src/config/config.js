8056ef86fe73318ea7645b03a1ecbeb3b7881821
AYUSHMAN THAKUR <141339858+THAKURayushman@users.noreply.github.com>
2025-05-24 14:27:46 +0530

    Update config.js

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -1,7 +1,7 @@
 
 export const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
+    secure: true,
+    sameSite: "None",
     maxAge: 1000 * 60 * 60, // 5 minutes
 }

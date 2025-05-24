faa7852bbc3295e76fd13a33e37a337eaff06589
AYUSHMAN THAKUR <141339858+THAKURayushman@users.noreply.github.com>
2025-05-24 14:44:15 +0530

    Update dashboard.js

--- Frontend/src/routing/dashboard.js
+++ Frontend/src/routing/dashboard.js
@@ -3,7 +3,7 @@ import { rootRoute } from "./routeTree"
 import DashboardPage from "../pages/DashboardPage"
 import { checkAuth } from "../utils/helper"
 
-export const dashboardRoute = createRoute({
+export const dasboardRoute = createRoute({
     getParentRoute: () => rootRoute,
     path: '/dashboard',
     component: DashboardPage,

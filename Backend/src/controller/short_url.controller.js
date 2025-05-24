5d1d6d10042aedf40ac64a0e0ea0d71f0b0bbb10
AYUSHMAN THAKUR <141339858+THAKURayushman@users.noreply.github.com>
2025-05-24 10:23:45 +0530

    Update short_url.controller.js

--- Backend/src/controller/short_url.controller.js
+++ Backend/src/controller/short_url.controller.js
@@ -1,4 +1,4 @@
-import { getShortUrl } from "../dao/short_url.js"
+import { getShortUrl } from "../Dao/short_url.js"
 import { createShortUrlWithoutUser, createShortUrlWithUser } from "../services/short_url.service.js"
 import wrapAsync from "../utils/tryCatchWrapper.js"
 
